280fc851da133a05d3df8f50d920b71476be5b43
Serge <lionheart.sergex@gmail.com>
2020-09-12 15:47:05 -0700

    Added keyboard controls

--- dist/Movement.js
+++ dist/Movement.js
@@ -1,127 +1,212 @@
-/* for offset to work appropriately with left and right 
-offset needs to match with margin lef in CSS file except for the offsets with
-single digits. This is important if you change the box's 
-default starting position on the screen  */
 let newSpeed;
-// document.body.addEventListener("load", setTimeout(disappear,4500));
-setTimeout(disappear,4500);
+let counter = 0;
+let timer;
+let numinput = document.getElementById("number");
+let instructions = document.getElementById("Teext");
+let boxer = document.getElementById("boxKun");
+let changeTimer;
 
+boxer.addEventListener("keydown", function(e){
+    // console.log(e.key);
+    KeyContext(e.key);
+});
+//Sets up scrolling text for scroll text and disappear function.
+const nextText =
+    [
+        "Box-kun can be moved either with control box buttons or keyboard",
+        "Use the speed box below and enter a number to change box-kun's speed",
+        "To move box-kun with keyboard use the 'W' 'A' 'S' 'D' keys", 
+        "Also click on the version text to see the history of this webpage"
+    ];
 
-let buttons=document.getElementsByTagName("button");
-//Loop through all buttons  to assign an event handler
-for (i =0; i < buttons.length-1; i++)
-{
-    buttons[i].addEventListener("click",function(){
+setTimeout(disappear, 300);
+let buttons = document.getElementsByTagName("button");
+
+//Loop through all buttons to assign an event handler
+for (i = 0; i < buttons.length - 1; i++) {
+    buttons[i].addEventListener("click", function () {
         BtnContext(this.id);
     });
-    
+
 }
 
-document.getElementById("Reset").addEventListener("click",Reset);
-let input=document.getElementById("number");
-input.addEventListener("input",function()
-{
-    realSpeed(this.value)
+document.getElementById("Reset").addEventListener("click", Reset);
+numinput.addEventListener("input", function () {
+    clearTimeout(changeTimer);
+    realSpeed(this.value);
+    if (numinput.value) { changeTimer = setTimeout(FocusChange, 550); }
+
 });
 
+
+
 function BtnContext(id) {
-    let box= document.getElementById("boxKun");
-        switch (id) {
-            case "rightMove":
-                var Offset1 =  550;
-                box.style.left= (box.offsetLeft + newSpeed - Offset1) + 'px';
-                Check(box);
-                break;
-            case "leftMove":
-                var Offset1 =  550;
-                box.style.left= (box.offsetLeft - newSpeed - Offset1) + 'px';
-                Check(box);
-                break;
-            case "upMove":
-                box.style.top= (box.offsetTop - newSpeed ) + 'px';
-                Check(box);                
-                break;
-            case "downMove":
-                var Offset1 =  1;
-                box.style.top= (box.offsetTop + newSpeed + Offset1) + 'px';
-                Check(box);                
-                break;
-            case "uprightMove":
-                var Offset2 =  550;
-                box.style.top= (box.offsetTop - newSpeed ) + 'px';
-                box.style.left= (box.offsetLeft + newSpeed - Offset2) + 'px';
-                Check(box);                                
-                break;
-            case "downrightMove":
-                var Offset1 =  1;
-                var Offset2 =  550;
-                box.style.top= (box.offsetTop + newSpeed + Offset1) + 'px';
-                box.style.left= (box.offsetLeft + newSpeed - Offset2) + 'px';
-                Check(box);                                
-                break;
-            case "upleftMove":
-                var Offset2 =  550;
-                box.style.top= (box.offsetTop - newSpeed ) + 'px';
-                box.style.left= (box.offsetLeft - newSpeed - Offset2) + 'px';
-                Check(box);                                
-                break;
-            case "downleftMove":
-                var Offset1 =  1;
-                var Offset2 =  550;
-                box.style.top= (box.offsetTop + newSpeed + Offset1) + 'px';
-                box.style.left= (box.offsetLeft - newSpeed - Offset2) + 'px';
-                Check(box);                                
-                break;
-                
-                default:
-                let text=document.getElementById("Teext");
-                text.innerHTML="This did not work";
-                break;
-                } 
-             }
+    // let boxer = document.getElementById("boxKun");
+    switch (id) {
+        case "rightMove":
+            boxer.style.left = (boxer.offsetLeft + newSpeed) + 'px';
+            Check(boxer);
+            boxer.focus();
+            break;
+        case "leftMove":
+            boxer.style.left = (boxer.offsetLeft - newSpeed) + 'px';
+            Check(boxer);
+            boxer.focus();
+            break;
+        case "upMove":
+            boxer.style.top = (boxer.offsetTop - newSpeed) + 'px';
+            Check(boxer);
+            boxer.focus();
+            break;
+        case "downMove":
+            boxer.style.top = (boxer.offsetTop + newSpeed) + 'px';
+            Check(boxer);
+            boxer.focus();
+            break;
+        case "uprightMove":
+            boxer.style.top = (boxer.offsetTop - newSpeed) + 'px';
+            boxer.style.left = (boxer.offsetLeft + newSpeed) + 'px';
+            Check(boxer);
+            boxer.focus();
+            break;
+        case "downrightMove":
+            boxer.style.top = (boxer.offsetTop + newSpeed) + 'px';
+            boxer.style.left = (boxer.offsetLeft + newSpeed) + 'px';
+            Check(boxer);
+            boxer.focus();
+            break;
+        case "upleftMove":
+            boxer.style.top = (boxer.offsetTop - newSpeed) + 'px';
+            boxer.style.left = (boxer.offsetLeft - newSpeed) + 'px';
+            Check(boxer);
+            boxer.focus();
+            break;
+        case "downleftMove":
+            boxer.style.top = (boxer.offsetTop + newSpeed) + 'px';
+            boxer.style.left = (boxer.offsetLeft - newSpeed) + 'px';
+            Check(boxer);
+            boxer.focus();
+            break;
+
+        default:
+            let text = document.getElementById("Teext");
+            text.innerHTML = "This did not work";
+            break;
+    }
+}
+function KeyContext(key) {
+    // let boxer = document.getElementById("boxKun");
+    switch (key) {
+        case "d":
+            //rightMove
+            boxer.style.left = (boxer.offsetLeft + newSpeed) + 'px';
+            Check(boxer);
+            break;
+        case "a":
+            //leftMove
+            boxer.style.left = (boxer.offsetLeft - newSpeed) + 'px';
+            Check(boxer);
+            break;
+        case "w":
+            //upMove
+            boxer.style.top = (boxer.offsetTop - newSpeed) + 'px';
+            Check(boxer);
+            break;
+        case "s":
+            //downMove
+            boxer.style.top = (boxer.offsetTop + newSpeed) + 'px';
+            Check(boxer);
+            break;
+        case "w" && "d":
+            //uprightMove
+            boxer.style.top = (boxer.offsetTop - newSpeed) + 'px';
+            boxer.style.left = (boxer.offsetLeft + newSpeed) + 'px';
+            Check(boxer);
+            break;
+        case "s" && "d":
+            //downrightMove
+            boxer.style.top = (boxer.offsetTop + newSpeed) + 'px';
+            boxer.style.left = (boxer.offsetLeft + newSpeed) + 'px';
+            Check(boxer);
+            break;
+        case "w" && "a":
+            //upleftMove
+            boxer.style.top = (boxer.offsetTop - newSpeed) + 'px';
+            boxer.style.left = (boxer.offsetLeft - newSpeed) + 'px';
+            Check(boxer);
+            break;
+        case "s" && "a":
+            //downleftMove
+            boxer.style.top = (boxer.offsetTop + newSpeed) + 'px';
+            boxer.style.left = (boxer.offsetLeft - newSpeed) + 'px';
+            Check(boxer);
+            break;
+
+        default:
+            let text = document.getElementById("Teext");
+            text.style.display= "block";
+            text.innerHTML = "This did not work";
+            break;
+    }
+}
 //function for converting user speed input to a numbered value
 function realSpeed(Val) {
     newSpeed = Number(Val);
     return newSpeed;
 }
 
-function Reset(params) {
-    var box= document.getElementById("boxKun");
-    box.style.top= "103px";
-    box.style.left= "0px";
-    box.style.backgroundImage="url(Images/DefaultFace.png)";
+function Reset() {
+    // var boxer = document.getElementById("boxKun");
+    boxer.style.top = "203px";
+    boxer.style.left = "503px";
+    boxer.style.backgroundImage = "url(Images/DefaultFace.png)";
+    boxer.focus();
 
 }
 
 function Check(Boxy) {
-    let boxSide= Boxy.offsetLeft + Boxy.offsetWidth;
-    let boxSide2= Boxy.offsetTop + Boxy.offsetHeight;
-    let btnContainer= document.getElementById("buttonContainer");
+    let boxSide = Boxy.offsetLeft + Boxy.offsetWidth;
+    let boxSide2 = Boxy.offsetTop + Boxy.offsetHeight;
+    let btnContainer = document.getElementById("buttonContainer");
     // let btnwLocation= btnContainer.offsetHeight + btnContainer.offsetTop;
-    if (boxSide >= (document.body.offsetWidth-100)) {
-        Boxy.style.backgroundImage="url(Images/OcrapRight.png)";
-    } else if (boxSide<= 195) {
-        Boxy.style.backgroundImage="url(Images/OcrapLeft.png)";
-        
-    } else if(boxSide2 > 904){
-        Boxy.style.backgroundImage="url(Images/OcrapDown.png)";
-        
-    } else if (boxSide2< 250){
-        Boxy.style.backgroundImage="url(Images/OcrapUp.png)";
+    if (boxSide >= (document.body.offsetWidth - 100)) {
+        Boxy.style.backgroundImage = "url(Images/OcrapRight.png)";
+    } else if (boxSide <= 195) {
+        Boxy.style.backgroundImage = "url(Images/OcrapLeft.png)";
+
+    } else if (boxSide2 > 904) {
+        Boxy.style.backgroundImage = "url(Images/OcrapDown.png)";
+
+    } else if (boxSide2 < 250) {
+        Boxy.style.backgroundImage = "url(Images/OcrapUp.png)";
     }
-      else if (boxSide <= 529 && boxSide2 >=853) {
-        Boxy.style.backgroundImage="url(Images/Derpface.png)";
-      }  
-   
-     else{
-        Boxy.style.backgroundImage="url(Images/DefaultFace.png)";
-        
+    else if (boxSide <= 529 && boxSide2 >= 853) {
+        Boxy.style.backgroundImage = "url(Images/Derpface.png)";
+    }
+
+    else {
+        Boxy.style.backgroundImage = "url(Images/DefaultFace.png)";
+
     }
 
 }
 
-function disappear(params) {
-     document.getElementById("Teext").style.display="none";
-     Reset();
-    
+function disappear() {
+    timer = setInterval(scrollText, 3800);
+}
+
+function scrollText() {
+    if (counter < 4) {
+        instructions.innerHTML = nextText[counter];
+        counter++
+    } else {
+        clearInterval(timer);
+        instructions.style.display = "none";
+        Reset();
+
+    }
+}
+
+function FocusChange() {
+    boxer.focus();
 }
